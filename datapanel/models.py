2287af1a1a652fe358d196d1ba7b92cb22a4d14d
kimichuan <kimchan@gmail.com>
2012-10-13 15:19:08 +0800

    action

--- datapanel/models.py
+++ datapanel/models.py
@@ -20,16 +20,17 @@ class Project(models.Model):
         unique_together = (('name', 'creator'),)
         ordering = ['-lastview']
 
+
 class Session(models.Model):
     project = models.ForeignKey(Project, related_name='session')
-    sn = models.CharField(unique=True,max_length=40, verbose_name=u'用户会话',default='')
+    sn = models.CharField(unique=True, max_length=40, verbose_name=u'用户会话', default='')
     start_time = models.DateTimeField(auto_now_add=True, verbose_name=u'会话开始时间')
     end_time = models.DateTimeField(auto_now=True, verbose_name=u'会话结束时间')
     user_language = models.CharField(max_length=255, verbose_name=u'客户端语言', default='')
     user_timezone = models.CharField(max_length=255, verbose_name=u'客户端时区', default='')
     user_agent = models.CharField(max_length=255, verbose_name=u'客户端类型', default='')
     user_referer = models.CharField(max_length=255, verbose_name=u'客户端来源', default='')
-    ipaddress = models.IPAddressField(verbose_name=u'IP地址',null=False,default='0.0.0.0')
+    ipaddress = models.IPAddressField(verbose_name=u'IP地址', null=False, default='0.0.0.0')
 
     def first_track(self):
         try:
@@ -42,20 +43,22 @@ class Session(models.Model):
             return self.track.filter().order_by('-dateline')[0]
         except IndexError:
             return None
-#
-#class Action(models.Model):
-#    project = models.ForeignKey(Project, related_name='action')
-#    name = models.CharField(max_length=255, verbose_name=u'动作')
-#    url = models.CharField(max_length=255, verbose_name=u'url')
-#    xpath = models.CharField(max_length=255, verbose_name=u'dom')
-#    event = models.CharField(max_length=255, verbose_name=u'event')
+
+
+class Action(models.Model):
+    project = models.ForeignKey(Project, related_name='action')
+    name = models.CharField(max_length=255, verbose_name=u'动作')
+    url = models.CharField(max_length=255, verbose_name=u'url')
+    xpath = models.CharField(max_length=255, verbose_name=u'dom')
+    event = models.CharField(max_length=255, verbose_name=u'event')
+
 
 class Track(models.Model):
     """
     User behavior action track
     """
     session = models.ForeignKey(Session, related_name='track', verbose_name=u'用户会话')
-    action = models.CharField( max_length=255,verbose_name=u'事件',default='')
+    action = models.CharField(max_length=255, verbose_name=u'事件', default='')
     url = models.CharField(max_length=255, verbose_name=u'url', default='')
     xpath = models.CharField(max_length=255, verbose_name=u'dom', default='')
     event = models.CharField(max_length=255, verbose_name=u'event', default='')
@@ -77,7 +80,8 @@ class Track(models.Model):
         if self.dateline:
             self.hourline = self.dateline.replace(minute=0, second=0, microsecond=0)
             self.dayline = self.dateline.replace(hour=0, minute=0, second=0, microsecond=0)
-            self.weekline = self.dateline.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=self.dateline.weekday())
+            self.weekline = self.dateline.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(
+                days=self.dateline.weekday())
             self.monthline = self.dateline.replace(day=1, minute=0, second=0, microsecond=0)
             if save:
                 self.save()
@@ -94,14 +98,14 @@ class Track(models.Model):
                 if parsed_url.netloc:
                     param['referer_site'] = parsed_url.netloc
                     param['referer_parsed'] = True
-                    querystring = urlparse.parse_qs(parsed_url.query,True)
-                    if parsed_url.netloc.find('baidu')!=-1:
+                    querystring = urlparse.parse_qs(parsed_url.query, True)
+                    if parsed_url.netloc.find('baidu') != -1:
                         #baidu
                         if querystring.has_key('wd'):
                             param['referer_keyword'] = smart_decode(querystring['wd'][0])
                         elif querystring.has_key('word'):
                             param['referer_keyword'] = smart_decode(querystring['word'][0])
-                    if parsed_url.netloc.find('sougou')!=-1:
+                    if parsed_url.netloc.find('sougou') != -1:
                         #sougou
                         if querystring.has_key('query'):
                             param['referer_keyword'] = smart_decode(querystring['query'][0])
