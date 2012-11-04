cd17eeb1a37a49070f4eaab7bfccf39507298700
zx-h <zhouxin262@qq.com>
2012-11-04 13:13:53 +0800

    monitor test

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
                        url(r'^create/$', 'project.views.create', name='project_create'),
                        url(r'^(?P<id>\d+)/$', 'project.views.home', name='project_home'),
+                       url(r'^(?P<id>\d+)/monitor/$', 'project.views.monitor', name='project_monitor'),
                        url(r'^(?P<id>\d+)/delete/$', 'project.views.delete', name='project_delete'),
                        url(r'^(?P<id>\d+)/setting/$', 'project.views.setting', name='project_setting'),
 
--- project/views/__init__.py
+++ project/views/__init__.py
@@ -2,10 +2,11 @@
 from django.contrib.auth.views import redirect_to_login
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 
 from project.forms import ProjectForm
 from session.models import Session, SessionGroupByTime
+from track.models import Track
 
 
 def create(request):
@@ -34,6 +35,25 @@ def home(request, id):
     datetype = request.GET.get('datetype', 'hour')
     sbt = SessionGroupByTime.objects.filter(project=project, datetype=datetype)
     return render(request, 'project/index.html', {'project': project, 'sbt': sbt})
+
+
+def monitor(request, id):
+    try:
+        project = request.user.participate_projects.get(id=id)
+    except AttributeError:
+        return redirect_to_login(request.get_full_path())
+
+    if request.is_ajax():
+        e = datetime.now()
+        s = datetime.now() - timedelta(seconds=60)
+        if request.GET.get('track'):
+            y = Track.objects.filter(session_project = project, dateline__range=[s,e]).count()
+        elif request.GET.get('session'):
+            y = Session.objects.filter(project = project, dateline__range=[s,e]).count()
+        return HttpResponse(y)
+
+    return render(request, 'project/monitor.html', {'project': project,})
+
 
 
 def setting(request, id):
