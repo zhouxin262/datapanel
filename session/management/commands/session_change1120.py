22020477e241dbc90706f16c2dd8da4f936d1b7f
zhouxin <zhouin262@qq.com>
2012-11-21 09:46:22 +0800

    scripts
    left time fixed

--- session/management/commands/session_change1120.py
+++ session/management/commands/session_change1120.py
@@ -10,13 +10,16 @@ class Command(LabelCommand):
 
     def handle_label(self, label, **options):
         _s = datetime.now()
-        _c = Session.objects.filter(agent__isnull=True).order_by('id').count()
-        for i in range(0, _c, 3000):
+        _c = Session.objects.filter(agent__isnull=True).count()
+        _l = _c
+        while _l:
             used_time = (datetime.now() - _s).seconds
             if used_time:
-                print used_time, 'used', round(float(_c - i) / (float(i) / used_time), 2), 'left'
+                print used_time, 'used', round(float(_l) / (float(_c - _l) / used_time), 2), 'left'
 
-            for s in Session.objects.filter(agent__isnull=True).order_by('id')[i: i + 3000]:
+            for s in Session.objects.filter(agent__isnull=True)[:3000]:
                 s.set_user_agent(s.user_agent)
                 s.set_referrer(s.user_referrer)
                 # print s.agent_id
+
+            _l = Session.objects.filter(agent__isnull=True).count()
--- track/management/commands/track_change1121.py
+++ track/management/commands/track_change1121.py
@@ -10,12 +10,14 @@ class Command(LabelCommand):
 
     def handle_label(self, label, **options):
         _s = datetime.now()
-        _c = Track.objects.filter(referrer_site__isnull=True).order_by('id').count()
-        for i in range(0, _c, 3000):
+        _c = Track.objects.filter(referrer_site__isnull=True).count()
+        _l = _c
+        while _l:
             used_time = (datetime.now() - _s).seconds
-            if used_time and i:
-                print used_time, 'used', round(float(_c - i) / (float(i) / used_time), 2), 'left'
+            if used_time:
+                print used_time, 'used', round(float(_l) / (float(_c - _l) / used_time), 2), 'left'
 
-            for s in Track.objects.filter(referrer_site__isnull=True).order_by('id')[i: i + 3000]:
+            for s in Track.objects.filter(referrer_site__isnull=True).order_by('id')[:3000]:
                 s.set_referrer(s.get_value('referrer'))
                 # print s.agent_id
+            _l = Track.objects.filter(referrer_site__isnull=True).count()
