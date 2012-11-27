5bb793ec256f170571ec008e15b966ef80bbc5fc
zhouxin <zhouin262@qq.com>
2012-11-27 10:54:11 +0800

    session

--- datapanel/views/__init__.py
+++ datapanel/views/__init__.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from project.models import Project
 from session.models import Session
@@ -36,30 +37,6 @@ def server_info(request):
     return HttpResponse(html)
 
 
-def get_or_create_session(request, project):
-    # 客户服务器访问，可带客户的客户session_key
-    session_key = None
-    if request.GET.get('s', None):
-        session_key = request.GET.get('s')
-    else:
-        session_key = request.session.session_key
-    s = Session.objects.get_or_create(sn=session_key,
-                                      project=project)
-    if s[1]:
-        s[0].ipaddress = request.META.get('REMOTE_ADDR', '0.0.0.0')
-        s[0].set_user_agent(request.META.get('HTTP_USER_AGENT', ''))
-        s[0].user_timezone = request.META.get('TZ', '')
-
-        #try:
-        params = ast.literal_eval(request.GET.get('p', ''))
-        s[0].set_referrer(params['referrer'])
-        # except:
-        #     pass
-
-        s[0].save()
-    return s
-
-
 def t(request):
     response = HttpResponse(mimetype="application/x-javascript")
     if not request.session.session_key:
@@ -73,10 +50,8 @@ def t(request):
     if request.META.get('HTTP_REFERER', '').find(project.url) == -1 and not request.GET.get('DEBUG'):
         return response
 
-    s = get_or_create_session(request, project)
-
     if request.GET.get('t', ''):
-        session = s[0]
+        session = Session.objects.get(session_key=request.session[settings.TMP_SESSION_COOKIE_NAME])
         # filter actions
         try:
             prv_track = Track.objects.filter(session=session).order_by('-dateline')[0]
@@ -125,12 +100,6 @@ def t(request):
         if 'function' in params and params['function']:
             f = params['function']
             if f[0] == 'set_user':
-                s[0].username = f[1]['username']
-                s[0].save()
+                pass
 
-    if s[1]:
-        # 新开的session，在客户服务器上存一个，需要配合
-        response_data = 'jx.callback({mp_act:"set_session", mb_session_key: "%s"});' % s[0].sn
-        response.write(response_data)
-        return response
     return response
