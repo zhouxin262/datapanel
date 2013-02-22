4ec069eda1f4a87bd81e9b02d3d9d4e0fe28df18
zx-h <zhouxin262@qq.com>
2013-02-22 18:57:33 +0800

    orderinfo

--- ecshop/urls.py
+++ ecshop/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
    url(r'^(?P<id>\d+)/$', 'ecshop.views.overview', name='ecshop_overview'),
    url(r'^(?P<id>\d+)/(?P<timeline_id>\d+)/$', 'ecshop.views.report2', name='ecshop_report2'),
    url(r'^(?P<id>\d+)/$', 'ecshop.views.overview', name='ecshop_overview'),
+   url(r'^(?P<id>\d+)/orderinfo/$', 'ecshop.views.orderinfo', name='ecshop_orderinfo'),
 )
