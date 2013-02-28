0fbcb10523c9ee353b707cd1dca75ff43ee87a8d
zx-h <zhouxin262@qq.com>
2013-02-28 20:41:58 +0800

    drange[1] +

--- datapanel/models.py
+++ datapanel/models.py
@@ -13,13 +13,13 @@ class Timeline(models.Model):
         from datetime import timedelta
         drange = (self.dateline, self.dateline)
         if self.datetype == 'hour':
-            drange[1] += timedelta(seconds=3600)
+            drange[1] = drange[1] + timedelta(seconds=3600)
         elif self.datetype == 'day':
-            drange[1] += timedelta(seconds=3600 * 24)
+            drange[1] = drange[1] + timedelta(seconds=3600 * 24)
         elif self.datetype == 'week':
-            drange[1] += timedelta(seconds=3600 * 24 * 7)
+            drange[1] = drange[1] + timedelta(seconds=3600 * 24 * 7)
         elif self.datetype == 'month':
-            drange[1] += timedelta(months=1)
+            drange[1] = drange[1] + timedelta(months=1)
         return drange
 
     def has_time(self, time):
