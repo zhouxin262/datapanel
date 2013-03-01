600eefc2bab6d9938434dfcca675fb69dfac6684
zhouxin <zhouin262@qq.com>
2013-03-01 10:45:21 +0800

    r2

--- ecshop/urls.py
+++ ecshop/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^(?P<id>\d+)/$', 'ecshop.views.overview', name='ecshop_overview'),
-                       url(r'^(?P<id>\d+)/$', 'ecshop.views.report2', name='ecshop_report2'),
-                       url(r'^(?P<id>\d+)/$', 'ecshop.views.overview', name='ecshop_overview'),
+                       url(r'^(?P<id>\d+)/r2/$', 'ecshop.views.report2', name='ecshop_report2'),
                        url(r'^(?P<id>\d+)/orderinfo/$', 'ecshop.views.orderinfo', name='ecshop_orderinfo'),
                        )
